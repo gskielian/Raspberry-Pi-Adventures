ec5c38fa3e6623d2c4c1e53c0517e6380fd2e0f2
gskielian <gregory.kielian@gmail.com>
2014-01-09 12:41:16 -0800

    added the zip file and began setup_script.sh

---
+++ Ad-Hoc/setup_script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo apt-get install bridge-utils hostapd
+
+wget http://www.daveconroy.com/wp3/wp-content/uploads/2013/07/hostapd.zip
+
+
+echo "if successful press y"
+read command
+
+if [ ! command = "y" ] ; then
+  exit
+fi
+
+unzip hostapd.zip
+
+sudo mv /usr/sbin/hostapd /usr/sbin/hostapd.bak
+sudo ln -sf /usr/sbin/hostapd.edimax /usr/sbin/hostapd
+sudo chown root.root /usr/sbin/hostapd
+sudo chmod 755 /usr/sbin/hostapd
