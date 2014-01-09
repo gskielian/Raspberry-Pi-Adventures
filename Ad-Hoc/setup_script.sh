2576a9100bcf5863dbf4955ef1d630c3fab1b8a3
gskielian <gregory.kielian@gmail.com>
2014-01-09 12:54:40 -0800

    added checkpoints

--- Ad-Hoc/setup_script.sh
+++ Ad-Hoc/setup_script.sh
@@ -2,24 +2,30 @@
 
 sudo apt-get install bridge-utils hostapd
 
-wget http://www.daveconroy.com/wp3/wp-content/uploads/2013/07/hostapd.zip
+#wget http://www.daveconroy.com/wp3/wp-content/uploads/2013/07/hostapd.zip
 
 
-echo "if successful press y"
+echo "checkpoint 1"
 read command
 
-if [ ! command = "y" ] ; then
-  exit
-fi
-
 unzip hostapd.zip
+
+
+echo "checkpoint 2"
+read command
 
 sudo mv /usr/sbin/hostapd /usr/sbin/hostapd.bak
 sudo ln -sf /usr/sbin/hostapd.edimax /usr/sbin/hostapd
 sudo chown root.root /usr/sbin/hostapd
 sudo chmod 755 /usr/sbin/hostapd
 
+echo "checkpoint 3"
+read command
+
 sudo mv /etc/network/interfaces/ /etc/network/interfaces.bak
 sudo sh -c "cat etc.network.interfaces > /etc/network/interfaces"
+
+echo "checkpoint 4"
+read command
 
 sudo reboot
